76cf7e54dd75db967291bce7e92a6434fe08c125
ryan4summer <ryan4summer0527@gmail.com>
2021-06-24 20:49:26 -0400

    feat: update new assignments

--- tiantianXia/assignments/twopointers/lc16/lc16.js
+++ tiantianXia/assignments/twopointers/lc16/lc16.js
@@ -1,14 +1,98 @@
 // Problem Statement #
 // Given an array of unsorted numbers and a target number, find a triplet in the array whose sum is as close to the target number as possible, return the sum of the triplet. If there are more than one such triplet, return the sum of the triplet with the smallest sum.
 
+/**
+ * inputs:
+ * @param {*} arr: an unsorted array 
+ * @param {*} target_sum: an integer 
+ * output:
+ * @returns the sum that close to the target and should be the smaller one
+ * 
+ * steps:
+ *  - using three pointers the first pointer is to traverse all the elements inside the array and
+ *    left pointer is i + 1, right pointer is length - 1
+ *  - when the current sum is less than the target move the left and update the current sum, when it's greater than the ta
+ *    target move right pointer and update the current sum
+ *  - get the current sum the distance between target and currentsum find the smaller distance store in the result
+ *  - compare the result and get smaller one
+ * 
+ * varialbes: left, right: pointers. currentSum: store the sum of current elements. result: store the final result
+ *            minDistacnce: use to compare the minimum distance from the target by default is inifinity
+ * 
+ * sort the array
+ * a for loop traverse all the element
+ *    initialize left as i + 1 and right is length - 1
+ *    a while loop runs when left is less of equal than the right
+ *        addup the current sum with arr[i], arr[left], and arr[right]
+ * 
+ *        compare if the minDistance greater than the distance between target and currentSum
+ *              store the smaller distance to minDistance
+ *              store the current to the result
+ * 
+ *        compare the result to get the small one
+ *      
+ *        if the current sum is less than the targetsum
+ *            update the currentsum 
+ *            update the left pointer
+ *        if the current sum is greater than the targetSUm
+ *            update the currentSUm
+ *            update the rightPointer
+ *  return the result
+ * 
+ * edge cases: arr is [] => 0  
+ */
 const triplet_sum_close_to_target = function (arr, target_sum) {
   // TODO: Write your code here
-  return -1;
+  if(!arr.length) return 0;
+
+  let currentSum = 0,
+      result = Infinity;
+  
+  arr.sort((a, b) => a - b);
+
+  for(let i = 0; i < arr.length - 2; i++){
+    let left = i + 1,
+        right = arr.length - 1;
+    
+    while(left < right){
+        currentSum = arr[i] + arr[left] + arr[right];
+
+        if (Math.abs(currentSum - target_sum) === 0) return currentSum;
+        if (Math.abs(currentSum - target_sum) < Math.abs(result - target_sum)) result = currentSum;
+
+        if(currentSum < target_sum){
+          left ++;
+        }else{
+          right --;
+        }
+    }
+  }
+  return result;
 };
 
-console.log(triplet_sum_close_to_target([-2, 0, 1, 2], 2));
-console.log(triplet_sum_close_to_target([-3, -1, 1, 2], 1));
-console.log(triplet_sum_close_to_target([1, 0, 1, 1], 100));
+console.log(triplet_sum_close_to_target([-2, 0, 1, 2], 2)); // 1
+console.log(triplet_sum_close_to_target([-3, -1, 1, 2], 1)); // 0
+console.log(triplet_sum_close_to_target([1, 0, 1, 1], 100)); // 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Solution
 // -----
